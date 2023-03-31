9313f77e256793882f62f3ea5212697d15fcbab2
Pavithra Gajendran <pavithra0823satish@outlook.com>
2023-03-31 16:21:54 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,10 +3,10 @@ region = "ap-southeast-2"
 }
 
 resource "aws_instance" "AWSInstance" {
-     ami = "ami-07dba8e4546543211"
+     ami = "ami-076b75d50e423acfe"
 	 instance_type = "t2.micro"
 	 key_name = "Pemfile"
-	 security_groups = ["launch-wizard-2"]
+	 security_groups = ["launch-wizard-4"]
 	 tags = {
 	 Name = "tomcatservers"
 	 }
